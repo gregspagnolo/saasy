6e4d700cb9db8d1414a8c630d32480584f1b9319
gregspagnolo <dwdrummer@gmail.com>
2017-03-06 21:56:11 -0600

    Clean up routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,8 +14,8 @@ class ContactsController < ApplicationController
 		end
 	end
 
-	private
-		def contact_params
-			params.require(:contact).permit(:name, :email, :comments)
-		end
+private
+	def contact_params
+		params.require(:contact).permit(:name, :email, :comments)
+	end
 end
